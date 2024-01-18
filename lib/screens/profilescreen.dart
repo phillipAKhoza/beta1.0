58fb64da7d67b83a2843f66e5044e0688065f52b
PHILLIP26 <phillip26@discovery.co.za>
2024-01-18 10:55:08 +0200

    auth reads modify

--- lib/screens/profilescreen.dart
+++ lib/screens/profilescreen.dart
@@ -11,11 +11,13 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   String? userEmail = CurrentUser.getUserEmail();
   String? userUid = CurrentUser.getUserUid();
+  late Future<UserResult> authDbCall;
   @override
   void initState() {
     super.initState();
     userEmail = CurrentUser.getUserEmail();
     userUid = CurrentUser.getUserUid();
+    authDbCall = Authentication().userAuth();
   }
   @override
   Widget build(BuildContext context) {
@@ -29,7 +31,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
         automaticallyImplyLeading: false,
       ),
       body: FutureBuilder<UserResult>(
-        future: Authentication().userAuth(), // a previously-obtained Future<String> or null
+        future: authDbCall, // a previously-obtained Future<String> or null
         builder: (BuildContext context, AsyncSnapshot<UserResult> snapshot) {
           List<Widget> children;
           if (snapshot.hasData) {
